f85f29afdcf484b3c3d9204f3bb8a6e3cd05da18
juliano leonardo soares <julianoleonardosoares@hotmail.com>
2020-04-18 17:07:37 -0300

    finalizado o app

---
+++ app/javascript/screens/discovery/index.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer'
+import SectionWrapper from '../../components/common/section_wrapper'
+import Discovery from '../../components/discovery'
+
+const DiscoveryScreen = () => {
+  return (
+    <Fragment>
+      <SectionWrapper>
+        <Discovery />
+      </SectionWrapper>
+      <NavbarFooter/>
+    </Fragment>
+  )
+}
+
+export default DiscoveryScreen;
